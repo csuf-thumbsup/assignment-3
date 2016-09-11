bf5b1baa892c16dc3e5877049d2f8b8a29c35dcc
Rogelio Negrete <rogelio_negrete@live.com>
2016-09-10 19:57:01 -0700

    Done and Doner

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
-import re
+# CPSC 323
+# Group 11
+
 def get_data_list(filename):
     # get list of each line in the file
     datalist = open(filename).readlines()
@@ -11,13 +13,18 @@ def beautify(datalist):
         # check if line is empty space then skip
         if str_line.isspace():
             continue
+
         # if first 2 chars are // then skip over
         if str_line[:2] == '//':
             continue
+
         # inline comment somewhere near the end of the line
         elif '//' in str_line:
             # chop off that comment
             str_line = str_line[:str_line.index('//')]
+
+        # strip leading/following whitespace
+        str_line.strip()
 
         # replace terminals with spaces
         str_list = list(str_line)
@@ -25,14 +32,20 @@ def beautify(datalist):
             if char in [',', ';', '+', '-', '*', '/', '=']:
                 str_list[str_list.index(char)] = ' ' + str(char) + ' '
 
-        # rejoin list and now split by spaces so every char has proper format
+        # rejoin list and now split by spaces into another list
         str_list = ''.join(str_list).split()
 
-        # rejoin one last time with proper spacing
+        # rejoin list with spaces inbetween
         str_list = ' '.join(str_list)
 
-        # add beatufied str to text
-        beautified_text +=  str_list + '\n'
+        # split into a list by semicolons
+        str_list = str_list.split(';')
+
+        # reformat the line and add to beautified_text
+        for x in str_list:
+            # ignore empty elements
+            if x:
+                beautified_text += x.strip() + ' ;\n'
 
     return beautified_text
 
@@ -45,23 +58,3 @@ if __name__ == '__main__':
     beautified_text = beautify(datalist)
     save_changes(beautified_text)
     print('\n\n--------------------------------\n' + beautified_text)
-
-
-'''
-IGNORE
-#special case - look for multiple semicolons per line and add newline accordingly
-        rx = re.split('(;)', str_list)
-        print('rx: ')
-        print(rx)
-        y = ''
-        for el in rx:
-            if el != ';':
-                y += el
-            else:
-                y += el + '\n'
-        print('y og: ' + y)
-        y = ''.join(y).split()
-        y = ' '.join(y)
-
-        print('y mod: ' + y)
-'''
